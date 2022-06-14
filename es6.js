9bf52b6adff66deed8d5131a60bacb4a4fdc8441
balaji <balajibaskar210493@gmail.com>
2022-06-14 10:39:08 +0530

    topics

---
+++ es6.js
@@ -0,0 +1,16 @@
+// 1-Arrow Functions
+// 2-Default Parameters
+// 3-Template Literals
+// 4-Let and Const
+// 5-Classes
+// 6-Destructuring
+// 7-Ternary Operator
+// 8-Import / Export Module
+// 9-Async / Await
+// 10-Spread Operator / Rest Parameter
+// 11-map-filter-reduce
+// 12-property shorthand
+// 13-ES6 classes
+// 14-promise+async/await
+// 15-axios
+// 16-api
