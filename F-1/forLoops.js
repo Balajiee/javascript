b38dd4277ee4ee7e9bd0d03547a861e65fa14ac0
balaji <balajibaskar210493@gmail.com>
2022-06-16 11:32:45 +0530

    final commit

---
+++ F-1/forLoops.js
@@ -0,0 +1,73 @@
+// Loops
+
+// 1-forLoop
+// 2-whileLoop
+// 3-doWhileLoop 
+// 4-forInLoop
+// 5-forOfLoop
+
+//in javascript we have various kind of loops,all loops do the same  things,they repeat the action number of times 
+
+// forloop
+        //inline variable
+// for(initial expression=let i=0;conditions i<5;incrementExpression i++){
+    // then add one or more statement
+// }
+// i is short for index
+
+// example-1
+// for(let i=0;i<5;i++){
+//     console.log("hello World",i);
+// }
+// example-2
+
+// const name=[1,2,3,4,5,6,7,8,9]
+// for (let i = 0; name < name.length; i++) {
+//     const element = array[name];
+    
+// }
+// console.log(name);
+
+// example-3
+// for(i=0;i<5;i++){
+//     if(i%2 !==0) console.log(i)
+// }
+
+// for(i=0;i<=5;i++)
+
+//     if(i%2 !==0){                    
+//         console.log("odd"+i);
+//     }else{
+
+//         console.log("even"+i);
+//     }
+// }
+    //[ 0/2=0
+    // 1/2=1
+    // 2/2=0
+    // 3/2=1
+    // 4/2=0
+    // 5/2=1]  //works in this way
+
+// for(i=10;i>=1;i--){
+//     if(i%2 !==0){
+//         console.log("odd"+i);
+//     }else{
+
+//         console.log("even"+i);
+//     }
+// }
+
+
+// example-4
+// const years=[1991,1992,1993,1994,1995,1996,1997,1998,1999,2000]
+// for(i=0;i<years.length;i++){
+//     const elements=years[i]
+//     console.log("years",elements);
+// }
+
+// example-5
+for(i=0;i<=50;i++){
+    console.log("you are going to learn JS"+i);
+}
+
