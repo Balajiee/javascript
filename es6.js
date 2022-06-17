5e8d89006855660ea84b2e7dc6e3bebf0a7692ec
balaji <balajibaskar210493@gmail.com>
2022-06-17 09:53:15 +0530

    final commit

--- es6.js
+++ es6.js
@@ -2,7 +2,7 @@
 // 2-Default Parameters
 // 3-Template Literals
 // 4-Let and Const
-// 5-Classes
+// 5-ES6 Classes
 // 6-Destructuring
 // 7-Ternary Operator
 // 8-Import / Export Module
@@ -10,7 +10,6 @@
 // 10-Spread Operator / Rest Parameter
 // 11-map-filter-reduce
 // 12-property shorthand
-// 13-ES6 classes
 // 14-promise+async/await
 // 15-axios
 // 16-api
